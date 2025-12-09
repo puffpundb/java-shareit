7d7c0bbc580ef9772af8c97f9bead88326da5939
Danil Shirykalov <shirds@yandex.ru>
2025-12-09 22:31:31 +0500

    Добавление тестов

---
+++ server/src/test/java/ru/practicum/shareit/item/model/CommentTest.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.item.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+
+	@Test
+	void setCreatedTimeOnInsert_setsCreated() {
+		Comment comment = new Comment();
+
+		assertNull(comment.getCreated());
+		comment.setCreatedTimeOnInsert();
+		assertNotNull(comment.getCreated());
+	}
+}
